0549e5d49d9ef8d319179b269c639ee6cf64af4b
leo <antoniou.leo@gmail.com>
2020-09-16 13:43:57 +0300

    Added CommandContext to solution

--- src/RemembrallCommandsAPI/Controllers/CommandsController.cs
+++ src/RemembrallCommandsAPI/Controllers/CommandsController.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers {
     [Route ("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase {
+
+        private readonly CommandContext _context;
+        public CommandsController (CommandContext context) => _context = context;
+
+        //GET: api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get () {
-            return new string[] { "this", "is", "hard", "coded" };
+        public ActionResult<IEnumerable<Command>> GetCommandItems () {
+            return _context.CommandItems;
         }
     }
 }
--- src/RemembrallCommandsAPI/Startup.cs
+++ src/RemembrallCommandsAPI/Startup.cs
@@ -2,17 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace RemembrallCommandsAPI {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        //  providing us with the fabric to read config data from appsettings.json
+        public IConfiguration Configuration { get; }
+        public Startup (IConfiguration configuration) =>
+            Configuration = configuration;
+
         public void ConfigureServices (IServiceCollection services) {
+            // register our CommandContext class as a solution-wide DBContext and we point it to the
+            // connection string
+            services.AddDbContext<CommandContext> (opt => opt.UseNpgsql (Configuration.GetConnectionString ("PostgreSqlConnection")));
+
             services.AddControllers ();
         }
 
